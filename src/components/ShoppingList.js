2a6bc56d81940a0fafae598e189c8f6cf5ac82c9
Robert Gorz <rjgorz@gmail.com>
2023-02-01 12:19:02 -0600

    completed dark mode toggle, add/remove from cart, and filter features

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterBy, setFilterBy] = useState("All");
+
+  const filterItems = items.filter(item => {
+    if(filterBy === "All")
+      return true;
+    else
+      return item.category === filterBy;
+  })
+
+  function handleFilter(e) {
+    setFilterBy(e.target.value);
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filterItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
